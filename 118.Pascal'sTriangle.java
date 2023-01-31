7f92c3857990fa78b1037feeb22a15f0fa2c9478
Vũ Hòa Bình <89285687+VuHoaBinh@users.noreply.github.com>
2023-01-31 15:59:52 +0700

    update file

--- 118.Pascal'sTriangle.java
+++ 118.Pascal'sTriangle.java
@@ -35,4 +35,21 @@ public class Solution {
         return ans;
     }
 }
-
+// idea c++
+/*
+void printPascal(int n){
+	int arr[n][n];
+	for (int line = 0; line < n; line++)
+	{
+		for (int i = 0; i <= line; i++)
+		{
+            if (line == i || i == 0){
+                arr[line][i] = 1;
+            }
+            else
+                arr[line][i] = arr[line - 1][i - 1] + arr[line - 1][i];
+		    cout << arr[line][i] << " ";
+		}
+	}
+}
+*/
