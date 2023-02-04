81eb05a114b12a69ce37a4cf4d7d29fdf282c0ad
Vũ Hòa Bình <89285687+VuHoaBinh@users.noreply.github.com>
2023-02-04 22:11:56 +0700

    Add files via upload

---
+++ 112.PathSum.py
@@ -0,0 +1,47 @@
+'''Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path 
+such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+ 
+Example 1:
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+
+Example 2:
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+
+Example 3:
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: bool
+        """
+        def open(node,result):
+            if not node:
+                return False
+            result+=node.val
+            if not node.left and not node.right:
+                return result == targetSum
+            return (open(node.left, result) or open(node.right, result))
+        return open(root,0)
+
+    
